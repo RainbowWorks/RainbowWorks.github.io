de30759f3dfc90d2adddd20a390128915f3a0990
RainbowWorks <kuj4235@kakao.com>
2025-05-23 14:18:34 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,94 +1,102 @@
+
+
 $(function () {
 
+    // h2오고 p나오도록 하는 명령어
+    let tl = gsap.timeline();
 
-    const MVS = new Swiper('.main_visual_slide', {
-        loop: true,
-        parallax: true,
-        //effect: 'fade', /*이미지에fade주기(effect: 'fade',)*/
+    tl.from({}, {})
+    tl.from('#main_reserve img', { y: 140, opacity: 0 });
+    tl.from('#main_reserve h2', { y: 140, opacity: 0 });
+    tl.from('#main_reserve  p', { y: 140, opacity: 0 });
+    tl.from('#main_reserve  a', { y: 140, opacity: 0 });
+
+    const slide = new Swiper('.main_intro_slide', {
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+        },
         speed: 1000,
+        loop: true,
+
         pagination: {
-            el: '.main_visual .page',
+            el: '.page',
             clickable: true, /*클릭안되면 쓰면된다.*/
         },/*도트만들때필요함*/
 
-    });
-
-
-    const main_manual_slide = new Swiper('.main_manual_slide', {
-        loop: true,
-        slidesPerView: 3.5,
-        spaceBetween: 30,
-    });
-
-
-
-
-    $('.main_manual .step button').on('click', function () {
-        let idx = $(this).parent().index();
-        main_manual_slide.slideToLoop(idx);
     })
 
+    $('#full_wrap').fullpage({
+        fixedElements: '#header , .side_lnk, .to_top',
+        paddingTop: '96px',
+        anchors: ['intro', 'store', 'customer', 'menu', 'footer'],
 
-    /*autoplay.start*/
+        afterLoad: function (o, d, dr) {
 
-    const MBS = new Swiper('.main_brand_slide', {
+            $('.side_lnk li').removeClass('on')
+            $('.side_lnk li').eq(d - 1).addClass('on')
+        },
+
+
+        onLeave: function (o, d, dr) {
+            if (d == 2) {
+                tl.restart();
+            }
+            if (d == 2 || d == 3 || d == 4) {
+                $('#header').addClass('on')
+            } else {
+                $('#header').removeClass('on')
+            }
+        },
+        // 헤더 아래 내려갔을때 고정시키는 방법 onleave ~떠날 때, 떠났던 자리, 떠날때 가는거? 해당 번호가 찍히는 것?  떠나올자리, 한자리, 방향 순서...????
+        // d== 조건문
+    });
+
+    const main_menu_slide = new Swiper('.main_menu_slide', {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 3.5,
         loop: true,
-        effect: 'fade', /*이미지에fade주기(effect: 'fade',)*/
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 120,
+            modifier: 2,
+            slideShadows: true,
+        },
         pagination: {
-            el: '.main_brand .page',
-            clickable: true, /*클릭안되면 쓰면된다.*/
-        },/*도트만들때필요함*/
-        navigation: {
-            nextEl: '.main_brand .next',
-            prevEl: '.main_brand .prev',
-        },/*화살표*/
+            el: ".swiper-pagination",
+        },
     });
 
+    //  푸터 섹션이 보이면 헤더 숨기기
+    const header = document.querySelector('#header');
+    const mainMenu = document.querySelector('#main_menu');
+    const footer = document.querySelector('#footer');
 
-});
-
-
-
-$(function () {
-    $(window).on('scroll', function () {
-        let sct = $(window).scrollTop();
-
-        if (sct > 0) {
-            $('#header').addClass('on');
+    const observer = new IntersectionObserver(([entry]) => {
+        if (entry.isIntersecting) {
+            // 푸터 보이면 둘 다 숨김
+            header.style.display = 'none';
+            mainMenu.classList.add('menu-fade');
         } else {
-            $('#header').removeClass('on');
+            header.style.display = 'block';
+            mainMenu.classList.remove('menu-fade');
         }
+    }, {
+        threshold: 0.5
     });
+
+    observer.observe(footer);
+
 });
 
 $(function () {
-    $('.main_menu .tab_menu button').on('click', function () {
-        let idx = $(this).index(); // 클릭한 것의 번호를 가져와서 저장함
-        console.log(idx);
-
-        $('.main_menu .tab_menu button').removeClass('on');
-        $(this).addClass('on');
-
-        $('.main_menu .tab_menu_content .con').removeClass('on');
-        $('.main_menu .tab_menu_content .con').eq(idx).addClass('on');
-    });
-});
-
-
-$(function () {
-    $('.to_top').on('click', function () {
-        $('html, body').animate({ scrollTop: 0 })
-    });
-
-    $(window).on('scroll', function () {
-        //스크롤 된 양을 구함.
-        let sct = $(window).scrollTop();
-        console.log(sct)
-
-        if (sct > 200) {
-            $('.to_top').addClass('on')
-        } else {
-            $('.to_top').removeClass('on')
-        }
-    });
+    $('.mbtn').on('click', function () {
+        $(this).toggleClass('is-active')
+        $('.gnb').toggleClass('on')
+        /*active를 붙여도 된다. 일반적으로는 on 사용한다.*/
+        $('#header h1').toggleClass('on')
+    })
 });
